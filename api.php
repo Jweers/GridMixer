7e08ad5ab4599ffa63565db14b88ea1f4747af2b
Jweers <jonweers@yahoo.com>
2014-05-12 14:29:05 -0600

    Added call to fetch level information from the database

--- api.php
+++ api.php
@@ -1,55 +1,20 @@
 <?php
   /*
    * API.php
-   * Handles status update requests for the GDR
+   * Handles ajax requests and other API calls
    */
 
   //Initialize Key Classes
   require_once 'assets/boot.php';
-  require_once 'assets/gdrFunctions.php';
-
 
   $action = isset($_REQUEST['action'])? $_REQUEST['action'] : 'help';
 
   switch ($action){
-    case 'submit':
+    case 'getLevel':
       //Sanitize Inputs
-      $submissionType = isset($_REQUEST['o'])? sanitize($_REQUEST['o']) : 'individual';
-      $submissionObj = isset($_REQUEST['subJson'])? @json_decode($_REQUEST['subJson']) : '';
-      $gdrId = isset($_REQUEST['gdrId'])? intval(sanitize($_REQUEST['gdrId'])) : -1;
+      $id = isset($_REQUEST['id'])? sanitize($_REQUEST['id']) : 1;
       //Formulate Response
-      if ($submissionType == 'edit'){
-        $response = updateSubmission($gdrId, $submissionObj);
-      }else{
-        $response = saveSubmission($submissionType, $submissionObj);
-      }
-      $responseType = 'application/json';
-      break;
-    case 'load':
-    case 'getSubmissionById':
-      //Sanitize Inputs
-      $submissionId = isset($_REQUEST['id'])? sanitize($_REQUEST['id']) : -1;
-      //Formulate Response
-      $response = loadSubmission($submissionId);
-      $responseType = 'application/json';
-      break;
-    case 'getSubmissionsForUser':
-      //Sanitize Inputs
-      $userId = isset($_REQUEST['id'])? sanitize($_REQUEST['id']) : -1;
-      //Formulate Response
-      $response = getSubmissionsForUser($userId);
-      $responseType = 'application/json';
-      break;
-    case 'delete':
-      //Sanitize Inputs
-      $gdrId = isset($_REQUEST['id'])? intval(sanitize($_REQUEST['id'])) : -1;
-      //Formulate Response
-      $response = removeSubmission($gdrId);
-      $responseType = 'application/json';
-      break;
-    case 'searchIdx':
-      $lastModified = isset($_REQUEST['lastModified'])? sanitize($_REQUEST['lastModified']) : 0;
-      $response = searchIdx($lastModified);
+      $response = getLevel($id);
       $responseType = 'application/json';
       break;
     case 'help':
@@ -58,12 +23,38 @@
       $responseType = 'text/html';
       break;
   }
-  //Publish JSON
+  //Publish Response
   header("Content-type: $responseType");
   header('Cache-Control: no-store, no-cache, must-revalidate');
   echo ($responseType == 'application/json')? json_encode($response) : $response;
 
   //End API Logic
   die;
-
+  
+  //Support Functions
+  
+  /**
+   * Get level by level id
+   * Returns the level object in $reponse->result
+   * @param integer $levelId
+   * @return stdClass $response
+   */
+  function getLevel($id){
+    $id = intval($id);
+    $response = new stdClass();
+    $response->result = false;
+    //Fetch from db
+    $db = Database::obtain();
+    try {
+      $response->result = $db->levels->findOne(array('id'=>$id));
+    } catch (Exception $e){
+      $response->error = true;
+      $response->errorMsg = "Unable to find level $id";
+      global $logger;
+      if ($logger instanceof Logger){
+        $logger->error("Unable to find level $id\n$e");
+      }
+    }
+    return $response;
+  }
 ?>
